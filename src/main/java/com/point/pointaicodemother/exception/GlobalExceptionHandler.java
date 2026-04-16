d812939671aa5ac5ac4c8d7b18e22728963dd993
PointWu <wuyuhuiss@outlook.com>
2026-04-16 13:21:49 +0800

    add 响应包装类 + 全局异常处理器

---
+++ src/main/java/com/point/pointaicodemother/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.point.pointaicodemother.exception;
+
+import com.point.pointaicodemother.common.BaseResponse;
+import com.point.pointaicodemother.common.ResultUtils;
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Hidden
+@RestControllerAdvice
+@Slf4j
+// 全局异常处理类 ，用于处理所有异常
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
