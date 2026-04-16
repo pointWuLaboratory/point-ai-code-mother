70ea8af6ecc7b50c90cb1c734478f28e7852bfb1
pointWu <wuyuhuiss@outlook.com>
2026-04-16 10:51:58 +0800

    init

---
+++ src/main/java/com/point/pointaicodemother/PointAiCodeMotherApplication.java
@@ -0,0 +1,13 @@
+package com.point.pointaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointAiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointAiCodeMotherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/point/pointaicodemother/PointAiCodeMotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.point.pointaicodemother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointAiCodeMotherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
